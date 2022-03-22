851b24539fb81a60b448eff1fa60f3916b49007a
Алексей <stormwww@yandex.ru>
2022-03-22 20:04:25 +0300

    валидное решение на все тесты, кроме самого жесткого на скорость

--- test.py
+++ test.py
@@ -1,20 +1,53 @@
-import sys
+def determinant(matrix, mul):
 
-for line in sys.stdin:
-    line = line.rstrip()
+    width = len(matrix)
+    if width == 1:
+        return mul * matrix[0][0]
+    else:
+        sign = -1
+        answer = 0
+        for i in range(width):
+            m = []
+            for j in range(1, width):
+                buff = []
+                for k in range(width):
+                    if k != i:
+                        buff.append(matrix[j][k])
+                m.append(buff)
+            sign *= -1
+            answer = answer + mul * determinant(m, sign * matrix[0][i])
+    return answer
 
-    while True:
-        w = []
-        q = []
-        c = 0
-        for i in range(len(line)):
-            if line[i] not in w:
-                w.append(line[i])
-            elif i in w:
-                q.append(line[i])
-            if (q == w) and (line[i + 1] == ''):
-                print(line)
-                c = 1
-                break
-        if (c == 1) or (q != w):
-            break
+#test_matrix = [[1,1,1, 1],[16,8, 2, 4],[81,27, 3, 9],[256,64, 4, 16]]
+test_matrix = [[1,1,1, 1],[16,8, 2, 4],[81,27, 3, 9],[256,64, 4, 16]]
+
+print(determinant(test_matrix, 1))
+
+
+
+
+def det2(matrix):
+    return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+
+
+def minor(matrix, i, j):
+    tmp = [row for k, row in enumerate(matrix) if k != i]
+    tmp = [col for k, col in enumerate(zip(*tmp)) if k != j]
+    return tmp
+
+
+def determinant(matrix):
+    size = len(matrix)
+    if size == 2:
+        return det2(matrix)
+
+    return sum((-1) ** j * matrix[0][j] * determinant(minor(matrix, 0, j))
+               for j in range(size))
+
+
+m = [[2, 4, 1, 1],
+     [0, 2, 1, 0],
+     [2, 1, 1, 3],
+     [4, 0, 2, 3]]
+
+print(determinant(m))
--- test2.py
+++ test2.py
@@ -7,81 +7,95 @@
 # in practice a sequance can be completed in many ways, assume that the equation used is a Polynomial, and find the simplest(smallest degree) equation possible, that leaves you with a single unique solution.
 
 
-# seq = [6, 17, 88, 321, 866]
-# k = len(seq)
-# if len(seq) == 5:
-#     for p in range(-10, 21):
-#         for q in range(-10, 21):
-#             for z in range(-10, 21):
-#                 for v in range(-10, 21):
-#                     if (p + q + z + v == seq[1]-seq[0]) and (p * 16 + q * 8 + z * 4 + v * 2 == seq[2]-seq[0]) and\
-#                         (p * 81 + q * 27 + z * 9 + v * 3 == seq[3]-seq[0]) and (p * 256 + q * 64 + z * 16 + v * 4 == seq[4]-seq[0]):
-#                             quest = [(p * i ** (k - 1)) + (q * i ** (k - 2)) + (z * i ** (k - 3)) + (v * i ** (k - 4)) + seq[0] for i in range(10)]
-# print(quest)
+
+# Валидное решение на все тесты, кроме самого жесткого
+
+
+def determinant(matrix, mul):
+
+    width = len(matrix)
+    if width == 1:
+        return mul * matrix[0][0]
+    else:
+        sign = -1
+        answer = 0
+        for i in range(width):
+            m = []
+            for j in range(1, width):
+                buff = []
+                for k in range(width):
+                    if k != i:
+                        buff.append(matrix[j][k])
+                m.append(buff)
+            sign *= -1
+            answer = answer + mul * determinant(m, sign * matrix[0][i])
+    return answer
+
+seq = [5]
+b = [seq[i]-seq[0] for i in range(1, len(seq))]
+
+print(b)
+ls = []
+lst = []
+for j in range(1, len(seq)):
+    for i in range(1, len(seq)):
+        ls.append(j**(len(seq)-i))
+    lst.append(ls)
+    ls = []
+print(lst)
+
+x = determinant(lst, 1)
+print(x)
+
+korni = []
+for j in range(len(lst)):
+    lst_x = list(map(list, lst))
+    for i in range(len(lst)):
+        lst_x[i][j] = b[i]
+    korni.append((determinant(lst_x, 1)) / x)
+print(korni)
+
+
+n = 4
+k = len(seq) - 1
+count = 0
+for i in range(k):
+    count += (korni[i] * n ** (k - i))
+count += seq[0]
+print(int(count))
+
+
+# def solve_sequence(seq):
+#     def solution(n):
+#         k = len(seq)
+#         if len(seq) == 5:
+#             for p in range(-10, 21):
+#                 for q in range(-10, 21):
+#                     for z in range(-10, 21):
+#                         for v in range(-10, 21):
+#                             if (p + q + z + v == seq[1] - seq[0]) and (
+#                                     p * 16 + q * 8 + z * 4 + v * 2 == seq[2] - seq[0]) and \
+#                                     (p * 81 + q * 27 + z * 9 + v * 3 == seq[3] - seq[0]) and (
+#                                     p * 256 + q * 64 + z * 16 + v * 4 == seq[4] - seq[0]):
+#                                     quest = [(p * n ** (k - 1)) + (q * n ** (k - 2)) + (z * n ** (k - 3)) + (v * n ** (k - 4)) + seq[0]]
 #
-# seq = [1, 1, 1, 7]
-# k = len(seq)
-# if len(seq) == 4:
-#     for q in range(-10, 21):
-#         for z in range(-10, 21):
-#             for v in range(-10, 21):
-#                 if (q + z + v == seq[1]-seq[0]) and (q * 8 + z * 4 + v * 2 == seq[2]-seq[0]) and (q * 27 + z * 9 + v * 3 == seq[3]-seq[0]):
-#                     quest = [(q * i ** (k - 1)) + (z * i ** (k - 2)) + (v * i ** (k - 3)) + seq[0] for i in range(10)]
-# print(quest)
+#         elif len(seq) == 4:
+#             for q in range(-10, 21):
+#                 for z in range(-10, 21):
+#                     for v in range(-10, 21):
+#                         if (q + z + v == seq[1] - seq[0]) and (q * 8 + z * 4 + v * 2 == seq[2] - seq[0]) and (
+#                                 q * 27 + z * 9 + v * 3 == seq[3] - seq[0]):
+#                             quest = [(q * n ** (k - 1)) + (z * n ** (k - 2)) + (v * n ** (k - 3)) + seq[0]]
 #
+#         elif 1 < len(seq) < 4:
+#             quest = [((seq[1] - seq[0]) * n) + seq[0]]
 #
+#         elif len(seq) == 1:
+#             quest = [seq[0]]
 #
-# seq = [2, 4, 6]
-# if len(seq) <= 3:
-#     quest = [((seq[1] - seq[0]) * i) + seq[0] for i in range(10)]
-# print(quest)
+#         elif len(seq) == 0:
+#             quest = [0]
 #
-# seq = [5]
-# if len(seq) == 1:
-#     quest = [seq[0] for i in range(10)]
-# print(quest)
+#         return quest[0]
 #
-# seq = []
-# if len(seq) == 0:
-#     quest = [0 for i in range(10)]
-# print(quest)
-
-
-# Валидное решение на первые 10 тестов(степерь ниже 5)
-
-def solve_sequence(seq):
-    def solution(n):
-        k = len(seq)
-        if len(seq) == 5:
-            for p in range(-10, 21):
-                for q in range(-10, 21):
-                    for z in range(-10, 21):
-                        for v in range(-10, 21):
-                            if (p + q + z + v == seq[1] - seq[0]) and (
-                                    p * 16 + q * 8 + z * 4 + v * 2 == seq[2] - seq[0]) and \
-                                    (p * 81 + q * 27 + z * 9 + v * 3 == seq[3] - seq[0]) and (
-                                    p * 256 + q * 64 + z * 16 + v * 4 == seq[4] - seq[0]):
-                                quest = [
-                                    (p * n ** (k - 1)) + (q * n ** (k - 2)) + (z * n ** (k - 3)) + (v * n ** (k - 4)) +
-                                    seq[0]]
-
-        elif len(seq) == 4:
-            for q in range(-10, 21):
-                for z in range(-10, 21):
-                    for v in range(-10, 21):
-                        if (q + z + v == seq[1] - seq[0]) and (q * 8 + z * 4 + v * 2 == seq[2] - seq[0]) and (
-                                q * 27 + z * 9 + v * 3 == seq[3] - seq[0]):
-                            quest = [(q * n ** (k - 1)) + (z * n ** (k - 2)) + (v * n ** (k - 3)) + seq[0]]
-
-        elif 1 < len(seq) < 4:
-            quest = [((seq[1] - seq[0]) * n) + seq[0]]
-
-        elif len(seq) == 1:
-            quest = [seq[0]]
-
-        elif len(seq) == 0:
-            quest = [0]
-
-        return quest[0]
-
-    return solution
+#     return solution
