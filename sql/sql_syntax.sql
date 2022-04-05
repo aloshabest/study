1af5c72d1bdb0508136aac92b1ab038b990594a4
Алексей <stormwww@yandex.ru>
2022-04-05 15:20:36 +0300

    sql

---
+++ sql/sql_syntax.sql
@@ -0,0 +1,12 @@
+CREATE TABLE students (
+
+    name TEXT,
+    las_name TEXT,
+    age INTEGER
+
+);
+
+SELECT * FROM students;
+
+INSERT INTO students (name, las_name, age) VALUES ('Lil', 'Wane', '22');
+INSERT INTO students (name, las_name, age) VALUES ('Jane', 'None', '42');
