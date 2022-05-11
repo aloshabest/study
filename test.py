8f5f71ca50ffe7483a273c466b3c6ed91fb35c1b
Алексей <stormwww@yandex.ru>
2022-05-11 17:18:15 +0300

    test

--- test.py
+++ test.py
@@ -1,29 +1,20 @@
-def asks(k, n):
-    def ingredients(func):
-        def wrapper(*args, **kwargs):
-            num = input(n)
-            print(f"{k} = {num}")
-            func(num)
-        return wrapper
-    return ingredients
+def make_decart_point(x, y):
+    return {'x': x, 'y': y}
 
-def ask(k, n):
-    def ingredients(func):
-        def wrapper(*args, **kwargs):
-            num = input(n)
-            print(f"{k} = {num}")
-            print(func(*args, num))
-        return wrapper
-    return ingredients
-@asks('x', 'Enter first number: ')
-@ask('y', 'Enter second number: ')
-def calc(x, y):
-    return int(x) + int(y)
-
-calc()
+def make_segment(p1, p2):
+    start = p1
+    end = p2
+    return start, end
 
 
-# def calc(x, y):
-#     print(f"{x}+{y}={int(x)+int(y)}")
-#
-# print(calc())
+def get_mid_piont_of_segment(segment):
+    start = segment[0]
+    end = segment[1]
+    x = (start['x'] + end['x']) / 2
+    y = (start['y'] + end['y']) / 2
+
+    return {'x': x, 'y': y}
+
+
+segment = make_segment(make_decart_point(3, 2), make_decart_point(0, 0))
+print(get_mid_piont_of_segment(segment))
