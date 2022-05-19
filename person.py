00e46c893d43e0c3e33658ea00813fd2d3a5958a
Алексей <stormwww@yandex.ru>
2022-05-19 17:32:02 +0300

    подключение дескрипторов

--- person.py
+++ person.py
@@ -116,5 +116,101 @@ print(p.__dict__)
 
 
 
+#
+##
+# Подключение дескрипторов
+##
+#
 
 
+
+
+class Verify:
+
+    S_RUS = 'абвгдеёжзийклмнопрстуфхцчшщьыъэюя-'
+    S_RUS_UPPER = S_RUS.upper()
+
+    @classmethod
+    def verify_fio(cls, fio):
+        if type(fio) != str:
+            raise TypeError("ФИО должно быть строкой")
+
+        f = fio.split()
+        if len(f) != 3:
+            raise TypeError("Неверный формат записи ФИО")
+
+        letters = ascii_letters + cls.S_RUS + cls.S_RUS_UPPER
+        for s in f:
+            if len(s) < 1:
+                raise TypeError("В ФИО должен быть хотя бы один символ")
+            if len(s.strip(letters)) != 0:
+                raise TypeError("В ФИО можно использовать только буквенные символы и дефис")
+
+
+    @classmethod
+    def verify_old(cls, old):
+        if type(old) != int or old < 14 or old > 120:
+            raise TypeError("Возраст должен быть целым числом в диапазоне [14; 120]")
+
+
+    @classmethod
+    def verify_weight(cls, w):
+        if type(w) != float or w < 20:
+            raise TypeError("Вес должен быть вещественным числом от 20 и выше")
+
+
+    @classmethod
+    def verify_ps(cls, ps):
+        if type(ps) != str:
+            raise TypeError("Паспорт должен быть строкой")
+
+        s = ps.split()
+        if len(s) != 2 or len(s[0]) != 4 or len(s[1]) != 6:
+            raise TypeError("Неверный формат паспорта")
+
+        for p in s:
+            if not p.isdigit():
+                raise TypeError("Серия и номер паспорта должны быть числами")
+
+
+    def __set_name__(self, owner, name):
+        self.name = '_' + name
+
+
+    def __get__(self, instance, owner):
+        return getattr(instance, self.name)
+
+
+    def __set__(self, instance, value):
+        a = f'self.verify' + self.name
+        print(a)
+        exec(a)
+        # if self.name == '_fio':
+        #     print(self.name)
+        #     self.verify_fio(value)
+        setattr(instance, self.name, value)
+
+
+class Person2:
+
+    fio = Verify()
+    ps = Verify()
+    weight = Verify()
+    old = Verify()
+
+
+    def __init__(self, fio, old, ps, weight):
+        self.fio = fio
+        self.old = old
+        self.passport = ps
+        self.weight = weight
+
+
+
+
+p = Person2('Балакирев Дмитрий Владимирович', 50, '1234 567890', 50.0)
+p.old = 90
+p.passport = '4567 123456'
+p.weight = 70.0
+print(p.old)
+print(p.__dict__)
